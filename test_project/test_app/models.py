1eeb4d273a41f5f6b173d8342ce566068c21c0a8
ryn-cx <84663588+ryn-cx@users.noreply.github.com>
2023-12-28 07:26:04 -0800

    Added testing for get_or_new

--- test_project/test_app/models.py
+++ test_project/test_app/models.py
@@ -1,16 +1,17 @@
 """Models for test_app."""
+
 from django.db import models
 
-from great_django_family import GetOrNew
+from great_django_family import ModelWithGetOrNew, ModelWithId
 
 
 # Create your models here.
-class TestGetOrNew(GetOrNew):
+class ImplementedGetOrNew(ModelWithId, ModelWithGetOrNew):
     """Implemntation of a model using GetOrNew."""
 
     name = models.CharField(max_length=100)
 
-    class Meta:
+    class Meta:  # type: ignore  # noqa: PGH003 - Meta has false positives
         """Meta class for TestGetOrNew."""
 
         app_label = "test_app"
--- tests/test_all.py
+++ tests/test_all.py
@@ -1,11 +1,25 @@
-"""Test great-django-family."""
 from __future__ import annotations
 
+from io import StringIO
+
 import pytest
+from django.core.management import call_command
 from django.db import models
 
 from great_django_family import auto_unique
-from test_project.test_app.models import TestGetOrNew
+from test_project.test_app.models import ImplementedGetOrNew
+
+
+# Based on code from: https://stackoverflow.com/a/73588607
+# It's easier to test an actual implementation of an abstract model than it is to test the abstract model itself. One
+# problem is that you could modify the abstract model and those changes will not be reflected in the implemented model
+# until after migrations are made, to avoid this always check if any migrations are missing before running the tests.
+@pytest.mark.django_db()
+def test_for_missing_migrations() -> None:
+    """Test that there are no missing migrations."""
+    output = StringIO()
+    call_command("makemigrations", no_input=True, dry_run=True, stdout=output)
+    assert output.getvalue().strip() == "No changes detected"
 
 
 class TestAutoUnique:
@@ -15,7 +29,7 @@ class TestAutoUnique:
         """Test that auto_test works."""
 
         # These nested classes are reuqired for the test to represent how they would be used in an actual project
-        class ModelName:  # type: ignore  # noqa: PGH003 - This model is used implicitly in the test
+        class ModelName:  # type: ignore  # noqa: PGH003 - This model is accessed implicitly in the test
             class Meta:
                 assert auto_unique("field1", "field2") == models.UniqueConstraint(
                     fields=("field1", "field2"),
@@ -23,21 +37,22 @@ class TestAutoUnique:
                 )
 
 
-class TestIt:
-    """Test the GetOrNew mixin."""
+class TestModels:
+    """Test the Models."""
 
     @pytest.mark.django_db()
     def test_get_or_new(self) -> None:
         """Test that get_or_new works."""
         # Test for an object that does not exist
-        obj, missing = TestGetOrNew().get_or_new(name="test")
+        obj, missing = ImplementedGetOrNew.objects.get_or_new(name="test")
         assert missing is True
-        assert obj.name == TestGetOrNew(name="test").name
+        assert obj.name == ImplementedGetOrNew(name="test").name
 
-        # Save object for next test
+        # Save object for ne
         obj.save()
 
-        # Test for an object that already exists
-        obj2, missing = TestGetOrNew().get_or_new(name="test")
+        # Get the same object
+
+        obj2, missing = ImplementedGetOrNew.objects.get_or_new(name="test")
         assert missing is False
-        assert obj2.pk == obj.pk
+        assert obj2.id == obj.id
